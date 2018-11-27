082740f35b64ad3b9a5d9cf82f3e4f749275aad5
VasiliyPlatonov <vasiliyplat@gmail.com>
2018-11-27 17:08:40 +0300

    Added statistics

--- practical_4/src/main/java/io/vasiliyplatonov/AppStart.java
+++ practical_4/src/main/java/io/vasiliyplatonov/AppStart.java
@@ -10,16 +10,30 @@ import static java.io.File.separator;
 public class AppStart {
     private static final int N_VERTEX = 4;
 
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
+        int countGraph = 0;
         int i = 0;
+
+        long pathTime = 0L;
+        long pathBefore;
+        long pathAfter;
+        long before;
+        long time;
+
+        before = System.nanoTime();
         while (!(i == 5)) {
             DiGraph graph = (DiGraph) GraphFiller.randomFillingBigLatin(new DiGraph(), N_VERTEX);
+            countGraph++;
 
+            pathBefore = System.nanoTime();
             String path = graph.getEulerianPath();
+            pathAfter = System.nanoTime();
 
             if (!(path == null) && !path.isEmpty()) {
                 i++;
+                pathTime += pathAfter - pathBefore;
+
                 System.err.println("\ngraph № " + i);
                 System.err.println(graph);
                 System.err.println(path);
@@ -32,6 +46,11 @@ public class AppStart {
                 System.err.println("------------------------------");
             }
         }
+        time = System.nanoTime() - before;
 
+        System.err.println("Всего создано графов: " + countGraph);
+        System.err.println("Среднее время поиска эйлеровского цикла: " + pathTime / countGraph);
+        System.err.println("Было потрачено времени: " + (time));
+        System.err.flush();
     }
 }
